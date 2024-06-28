c6520da6f4f87df4ba06009f587c8372de252a37
= <=>
2024-06-28 18:13:52 +0300

    add verify_order webhook

--- routes/tax.js
+++ routes/tax.js
@@ -6,4 +6,6 @@ const taxController = require('../controllers/taxController')
 /* GET home page. */
 router.post('/calculate',taxController.calculateTaxes );
 
+router.post('/verify_order',taxController.verifyOrder );
+
 module.exports = router;
